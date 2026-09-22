09b7fe24a06e123f99b249d4636e0ea131b4f517
Harsh Vardhan Pal <hp77335@gmail.com>
2026-09-22 13:07:07 +0530

    Time: 13 ms (27.17%), Space: 63.6 MB (5.43%) - LeetHub

---
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var repeatedNTimes = function(nums) {
+let map  = new Map();
+for(let t of nums){
+  map.set(t,(map.get(t)||0)+1)
+};
+for(let [key,value] of map){
+  if(value == nums.length/2){
+    return key
+  };
+}
+};
